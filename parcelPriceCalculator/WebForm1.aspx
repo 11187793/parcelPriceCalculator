e6029aabf7a4f7a03149a5c0487b3ea422177d46
11187793@college.wlc.ac.uk <11187793@college.wlc.ac.uk>
2024-05-01 11:50:00 +0100

    Add project files.

---
+++ parcelPriceCalculator/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="parcelPriceCalculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <strong>Parcel Price Calculator </strong>
+            <br />
+            <br />
+            Width: <asp:TextBox ID="widthBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Height:
+            <asp:TextBox ID="heightBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Length:
+            <asp:TextBox ID="lengthBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadio" runat="server" GroupName="radioButtons" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="airRadio" runat="server" GroupName="radioButtons" Text="Air" />
+            <br />
+            <asp:RadioButton ID="nextDayRadio" runat="server" GroupName="radioButtons" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Button ID="calculateBtn" runat="server" OnClick="calculateBtn_Click" Text="Calculate " />
+&nbsp;<asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="errorLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ parcelPriceCalculator/WebForm1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace parcelPriceCalculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool sufficientData()
+        {
+            if (lengthBox.Text.Trim() == "")
+                lengthBox.Text = "1";
+
+            if (widthBox.Text.Trim() == "") return false;
+            else if (heightBox.Text.Trim() == "") return false;
+            else if (!groundRadio.Checked && !airRadio.Checked && !nextDayRadio.Checked) return false; 
+            else return true;
+        }
+
+
+        int[] getData()
+        {
+            // first index is width, second index is height, third index is volume
+
+            int[] data = { int.Parse(widthBox.Text.Trim()),  int.Parse(heightBox.Text.Trim()), int.Parse(lengthBox.Text.Trim()) };
+
+            return data;
+        }
+
+        int getVolume(int[] data)
+        {
+            return data[0] * data[1] * data[2];
+        }
+
+        double determineMultiplier()
+        {
+            double multiplier = 0;
+
+            if (groundRadio.Checked) multiplier = 0.15;
+            else if (airRadio.Checked) multiplier = 0.25;
+            else if (nextDayRadio.Checked) multiplier = 0.45;
+
+            return multiplier;
+        }
+
+        double getCost(int volume, double multiplier)
+        {
+            return volume * multiplier; 
+        }
+
+        protected void calculateBtn_Click(object sender, EventArgs e)
+        {
+            if (sufficientData() == false)
+            {
+                errorLabel.Text = "Insufficient Data";
+                resultLabel.Text = ""; 
+                return; 
+            } else
+            {
+                errorLabel.Text = "";
+            }
+
+            resultLabel.Text = String.Format("The cost is {0:C}", getCost(getVolume(getData()), determineMultiplier()));
+        }
+    }
+}
